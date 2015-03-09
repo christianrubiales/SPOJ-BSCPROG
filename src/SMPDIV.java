c6a65c1d9cb658a2646291a3805146cb788aa650
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-10 00:34:42 +0800

    SMPDIV

--- src/SMPDIV.java
+++ src/SMPDIV.java
@@ -1,9 +1,8 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
+/**
+ * Divisibility
+ */
 class SMPDIV {
 
     public static void main(String[] args) {
