7b2353691b8a825aec19546a8cea7e63035039be
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-09 21:07:23 +0800

    SMPSEQ6

---
+++ src/SMPSEQ6.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+class SMPSEQ6 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int x = in.nextInt();
+
+		int[] s = new int[n];
+		int[] q = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			s[i] = in.nextInt();
+		}
+		for (int i = 0; i < n; i++) {
+			q[i] = in.nextInt();
+		}
+
+		StringBuilder u = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			for (int j = i - x; j <= i + x; j++) {
+				if (j > -1 && j < n && s[i] == q[j]) {
+					u.append((i+1) + " ");
+					break;
+				}
+			}
+		}
+
+		System.out.println(u.toString().trim());
+	}
+
+}
